c3394cc7440147daa29677521ad490f485e7f874
teevik <teemu.vikoren@gmail.com>
2023-10-01 13:38:56 +0200

    Change `Texture.h` and `Texture.cpp` to use namespace framework

--- framework/include/framework/Texture.h
+++ framework/include/framework/Texture.h
@@ -5,35 +5,36 @@
 #include <string>
 #include "stb_image.h"
 
-struct Texture {
-    uint32_t id;
-    stbi_uc *pixels;
+namespace framework {
+    struct Texture {
+        uint32_t id;
+        stbi_uc *pixels;
 
-    void free() const;
+        void free() const;
 
-    void bind() const;
-};
+        void bind() const;
+    };
 
-enum class Filtering {
-    Nearest,
-    Linear
-};
+    enum class Filtering {
+        Nearest,
+        Linear
+    };
 
-enum class Wrapping {
-    Repeat
-};
+    enum class Wrapping {
+        Repeat
+    };
 
-Texture loadTexture(
-    const std::string &path,
-    Filtering filtering = Filtering::Linear,
-    Wrapping wrapping = Wrapping::Repeat
-);
+    Texture loadTexture(
+        const std::string &path,
+        Filtering filtering = Filtering::Linear,
+        Wrapping wrapping = Wrapping::Repeat
+    );
 
-Texture loadCubemap(
-    const std::string &path,
-    Filtering filtering = Filtering::Linear,
-    Wrapping wrapping = Wrapping::Repeat
-);
-
+    Texture loadCubemap(
+        const std::string &path,
+        Filtering filtering = Filtering::Linear,
+        Wrapping wrapping = Wrapping::Repeat
+    );
+}
 
 #endif //PROG2002_TEXTURE_H
