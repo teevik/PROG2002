d9604a070ea51cfb9ce1c03bc76dd52f49752dcd
teevik <teemu.vikoren@gmail.com>
2023-10-08 19:51:06 +0200

    Cleanup code for Example 6

--- examples/example_6/main.cpp
+++ examples/example_6/main.cpp
@@ -6,49 +6,50 @@
 #include "framework/geometry.h"
 #include "framework/Camera.h"
 
+// language=glsl
+const std::string vertexShaderSource = R"(
+    #version 450 core
+
+    layout(location = 0) in vec2 position;
+    layout(location = 1) in vec4 color;
+
+    out vec4 vertex_color;
+
+    uniform mat4 projection;
+
+    void main() {
+        gl_Position = projection * vec4(position.xy, 0.0, 1.0);
+        vertex_color = color;
+    }
+)";
+
+// language=glsl
+const std::string fragmentShaderSource = R"(
+    #version 450 core
+
+    out vec4 color;
+
+    const vec4 white = vec4(1, 1, 1, 1);
+    const vec4 black = vec4(0, 0, 0, 1);
+
+    void main() {
+        int index = gl_PrimitiveID/2;
+        int x = index % 8;
+        int y = index / 8;
+
+        ivec2 tile_index = ivec2(x, y);
+
+        bool is_black = tile_index.x % 2 == tile_index.y % 2;
+
+        color = is_black ? black : white;
+    }
+)";
+
 int main() {
     int width = 800;
     int height = 600;
 
     auto window = framework::createWindow(width, height, "Example 6");
-
-    // language=glsl
-    const std::string vertexShaderSource = R"(
-        #version 450 core
-
-        layout(location = 0) in vec2 position;
-        layout(location = 1) in vec4 color;
-
-        out vec4 vertex_color;
-
-        uniform mat4 projection;
-
-        void main() {
-            gl_Position = projection * vec4(position.xy, 0.0, 1.0);
-            vertex_color = color;
-        }
-    )";
-
-    // language=glsl
-    const std::string fragmentShaderSource = R"(
-        #version 450 core
-
-        out vec4 color;
-
-        const vec4 white = vec4(1, 1, 1, 1);
-        const vec4 black = vec4(0, 0, 0, 1);
-
-        void main() {
-            int index = gl_PrimitiveID/2;
-            int x = index % 8;
-            int y = index / 8;
-
-            ivec2 tile_index = ivec2(x, y);
-
-            bool is_black = tile_index.x % 2 == tile_index.y % 2;
-
-            color = is_black ? black : white;
-        })";
 
     auto grid = framework::generateGridMesh(8);
 
@@ -88,7 +89,7 @@ int main() {
         bool isPressingEscape = glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS;
         if (isPressingEscape) break;
     }
-    
+
     glfwTerminate();
 
     return EXIT_SUCCESS;
